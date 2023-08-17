5e18eba0a2c83b58b4ba141f8ea818a65e30d4fe
rizkyachmadsyah <achmad.rizky@misteraladin.com>
2023-08-17 21:55:23 +0700

    remove build result

--- .output/public/_nuxt/funded-illustration.ea35f2fb.js
+++
@@ -1,1 +0,0 @@
-import"./entry.3e678935.js";const t=""+globalThis.__publicAssetsURL("funded-illustration.svg");export{t as _};
--- .output/public/_nuxt/icon-avatar-add.391090ef.js
+++
@@ -1,1 +0,0 @@
-import"./entry.3e678935.js";const a=""+globalThis.__publicAssetsURL("icon-avatar-add.svg");export{a as _};
